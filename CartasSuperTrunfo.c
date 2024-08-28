195f02327b27194e7056e02883620715bbb4a86f
Ggwgremio <guilhermegarith@hotmail.com>
2024-08-28 23:45:15 +0000

    atualizei o super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,9 +3,10 @@
 int main() {
     char codigo[4];           // Código da carta (Ex: A01, B02)
     int Populacao = 1;        // População da cidade
-    float Area = 1;           // Área da cidade em km²
-    float PIB = 1;            // PIB da cidade em bilhões
+    float Area = 1.5;           // Área da cidade em km²
+    float PIB = 1.5;            // PIB da cidade em bilhões
     int PontosTuristicos = 1; // Número de pontos turísticos
+    float divisao;
 
     printf("Digite o codigo da carta: ");
     scanf("%s", codigo);
@@ -21,6 +22,12 @@ int main() {
 
     printf("Digite o numero de pontos turisticos da cidade: ");
     scanf("%d", &PontosTuristicos);
+    
+    divisao = (float) Populacao / Area;
+    printf("A Densidade populacional é: %.2f\n", divisao);
+
+    divisao = (float) PIB / Populacao;
+    printf("O PIB per capita é: %.2f\n", divisao);
 
     return 0;
 
